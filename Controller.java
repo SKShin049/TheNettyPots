568183b2374fbb32764fc5f567f2c15fbc8a19b3
ryancperez <85649432+ryancperez@users.noreply.github.com>
2023-08-09 12:03:04 -0700

    GUI
    This is all of the gui pages I've worked on so far. if you insert these files into your "hellofx" it should run!

---
+++ Controller.java
@@ -0,0 +1,67 @@
+package hellofx;
+
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    void OpenMenu(ActionEvent event) throws IOException{
+        String FoodMenufile = "FoodMenuScene.fxml";
+        changeScene(FoodMenufile, event);
+    }
+
+    @FXML
+    void OpenStoreInfo(ActionEvent event) throws IOException{
+        String StoreInfofile = "StoreInfoScene.fxml";
+        changeScene(StoreInfofile, event);
+    }
+
+    @FXML
+    void StartDelivery(ActionEvent event) throws IOException{
+        String StartDeliveryFile = "StartDeliveryScene.fxml";
+        changeScene(StartDeliveryFile, event);
+    }
+
+    @FXML
+    void StartTakeout(ActionEvent event) throws IOException{
+        String StartTakeoutfile = "StartTakeoutScene.fxml";
+        changeScene(StartTakeoutfile, event);
+    }
+
+    @FXML
+    void viewAccount(ActionEvent event) throws IOException{
+        changeScene("AccountScene.fxml", event);
+    }
+    
+    @FXML
+    void returnToHome(ActionEvent event) throws IOException{
+        changeScene("MainScene.fxml", event);
+    }
+
+    @FXML
+    void OpenCart(ActionEvent event) throws IOException{
+        changeScene("CartScene.fxml", event);
+    }
+
+    public void changeScene(String fxmlName, ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource(fxmlName));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
