e31394d7e73d2122e79a7fde17c94bc4675d996d
Ren_ <58993316+renzopereyra115@users.noreply.github.com>
2023-08-15 20:04:18 -0700

    Created Javadoc comments.

--- src/StoreInfo.java
+++ src/StoreInfo.java
@@ -1,3 +1,14 @@
+/**
+ * Class Name: StoreInfo
+ * Stores information about the business, such as Full Address and Phone Number.
+ * Date of Code: 08/10/2023
+ * @author Renzo Pereyra
+ */
+
+/** 
+ * Stores public information about the pizza store. 
+ * It does not contain any input values, however it may be used as a reference to output string values.
+ */
 public class StoreInfo {
     public static String Address= "18111 Nordoff St";
     public static String City = "Northridge";
