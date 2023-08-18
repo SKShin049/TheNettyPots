6743a93db93d522b3a6d10f09cd6c39bd79ba5f1
morganceili <morganceili@gmail.com>
2023-08-18 16:12:19 -0700

    testing FoodMenu.txt location

--- src/Order.java
+++ src/Order.java
@@ -15,8 +15,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class Order extends ShoppingCart {
+public class Order extends User {
     //note: this should inherit cart class
     double total;
     double deliveryFee;
@@ -37,12 +38,15 @@ public class Order extends ShoppingCart {
         this.orderTime = orderTime;
     }
 
-    //8/16/23 METHOD IS NOT COMPLETE
+    /*/8/16/23 METHOD IS NOT COMPLETE
     public void createOrder(){
         File customerOrder = new File("CustomerOrder.txt"); //creates customerOrder file object
         System.out.println(System.getProperty("user.dir"));
         try {
             FileWriter orderWriter = new FileWriter("CustomerOrder.txt");
+            //while(orderWriter.exists()){
+
+            //}
             orderWriter.write("testing");
             orderWriter.close();
         } 
@@ -52,7 +56,7 @@ public class Order extends ShoppingCart {
         }
         
         //TODO: generate customerOrder with items chosen in ShoppingCart.java
-    }
+    }*/
 
     public void readyAt(){
         //take order time and add 25 mins
@@ -61,7 +65,12 @@ public class Order extends ShoppingCart {
     public void checkRadius(int customerZip){
         int[] zipCodes = {91330, 91324, 91325, 91343, 91345, 91326, 91311, 91306, 91335, 91406}; //zips in the surrounding area that we can deliver to
         customer.zipCode = customerZip;
-        //TODO: check if zip is in the list
+        boolean inRange = Arrays.asList(zipCodes).contains(customerZip);
+        if(inRange == true){
+            System.out.println("in range");
+        } else {
+            System.out.println("out of range");
+        }
     }
 
     public void checkout(){
