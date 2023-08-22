255507165d18621f360a684cad3507c52eea4cc3
morganceili <morganceili@gmail.com>
2023-08-21 22:04:59 -0700

    added CheckoutController class

---
+++ src/CheckoutController.java
@@ -0,0 +1,39 @@
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+
+public class CheckoutController {
+
+    @FXML
+    void OpenCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OpenMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenStoreInfo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void returnToHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewAccount(ActionEvent event) {
+
+    }
+
+}
